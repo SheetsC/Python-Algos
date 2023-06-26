48469481e289c1cfda4ecbce207f9c9607c14fa0
Connor Sheets <connorsheets32@gmail.com>
2023-06-26 15:24:56 -0500

    abstract art

---
+++ Basics/abstracted.py
@@ -0,0 +1,20 @@
+# Abstraction is used for classes and methods that are not instantiated
+# rather they used as rules for instantiation of subclasses. abstract methods 
+# are intended to be overridden when making the subclassed and thier methods. 
+from abc import ABC, abstractmethod
+
+class Shape(ABC):
+    @abstractmethod
+    def area(self):
+        pass
+    @abstractmethod
+    def perimeter(self):
+        pass
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return 3.14 * self.radius * self.radius
+    def perimeter(self):
+        return 3.14 * 2 * self.radius
